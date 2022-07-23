a3e7babe5e59e9e86265e076ddde44b09bf90362
Shon Feder <shon@informal.systems>
2022-07-23 02:26:40 -0400

    Improve logic for waiting for server
    The previous test was not differentiating an error resulting from lack
    of a socket and an error resulting from a timeout. Since there may not
    be a socket created for the server when we start waiting, we need to
    keep trying up to our time limit even if the socket doesn't exist yet.

--- integration-tests/test_apalache_integration.py
+++ integration-tests/test_apalache_integration.py
@@ -4,27 +4,38 @@ from collections.abc import Iterator
 
 import pytest
 import socket
+import time
 
-from subprocess import PIPE, Popen
-
+from subprocess import Popen
 from chai import Chai
 
 
 # Utility function
-def ping_server(server: str, port: int, timeout: int = 3) -> bool:
+def wait_for_server(server: str, port: int, timeout: int) -> bool:
     """Ping a server
 
-    Taken from https://stackoverflow.com/a/67217558/1187277
+    Adapated from https://stackoverflow.com/a/67217558/1187277
     """
-    try:
-        socket.setdefaulttimeout(timeout)
+    remaining_time = timeout
+    while remaining_time > 0:
+        socket.setdefaulttimeout(remaining_time)
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((server, port))
-    except OSError:
-        return False
+        try:
+            s.connect((server, port))
+        except TimeoutError:
+            # We ran out of time waiting for a connection on the socket
+            return False
+        except OSError:
+            # We weren't able to connect to the socket, wait a bit, and try again
+            remaining_time -= 1
+            time.sleep(1)
+        else:
+            # We connected!
+            s.close()
+            return True
     else:
-        s.close()
-        return True
+        # We ran out of remaining_time, so give up
+        return False
 
 
 # Fixture to start and clean up Apalache's Shai server
@@ -42,20 +53,10 @@ def ping_server(server: str, port: int, timeout: int = 3) -> bool:
 @pytest.fixture(autouse=True, scope="module")
 def server() -> Iterator[Popen]:
     # TODO Pass port to server explicitly when that is supported
-    process = Popen(["apalache-mc", "server"], stdout=PIPE)
-    if process.stdout is None:
-        raise RuntimeError(
-            "No output from Apalache server, cannot confirm it's running"
-        )
-    for line in process.stdout:
-        if "The Apalache server is running." in line.decode("UTF-8"):
-            break
-    import time
-
-    time.sleep(10)
+    process = Popen(["apalache-mc", "server"])
     # Startup can take quite some time, especially on the CI machines
     timeout_secs = 60
-    if not ping_server(Chai.DEFAULT_DOMAIN, Chai.DEFAULT_PORT, timeout_secs):
+    if not wait_for_server(Chai.DEFAULT_DOMAIN, Chai.DEFAULT_PORT, timeout_secs):
         raise RuntimeError(
             f"Apalache server did not start after {timeout_secs} seconds"
         )
