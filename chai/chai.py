7d4d5f595db5d733d555120374c458f71f3ec8cc
Shon Feder <shon@informal.systems>
2022-07-26 16:39:05 -0400

    Use asyncio for the concurrent executions of RPC calls from the client to the server (#16)
    * Fix method case
    
    Also sort imports in test
    
    * Add pytest-asyncio dependency
    
    For testing our asyncio interface
    
    * Use asyncio for client
    
    Co-authored-by: Thomas Pani <thomas.pani@gmail.com>

--- chai/chai.py
+++ chai/chai.py
@@ -1,5 +1,9 @@
 """The gRPC client to interact with Apalache's Shai server"""
 
+# TODO: Mypy can't currently generate stubs for the grpc.aio code, so we have a
+# few `type: ignore` annotations scattered around. Remove these when
+# https://github.com/nipunn1313/mypy-protobuf/issues/216 is closed.
+
 # Postpone evaluation of annotations
 # see:
 #  - https://stackoverflow.com/a/33533514/1187277
@@ -7,16 +11,27 @@
 #
 from __future__ import annotations
 
-from types import TracebackType
-from typing import Optional, Type, TypeVar
-import grpc
+import asyncio
+from collections.abc import AsyncIterator, Awaitable
+from contextlib import asynccontextmanager
+from typing import Any, Optional, TypeVar
+
+# TODO remove `type: ignore` when stubs are available for grpc.aio See
+# https://github.com/shabbyrobe/grpc-stubs/issues/22
+import grpc.aio as aio  # type: ignore
+from grpc import ChannelConnectivity
+
 import chai.transExplorer_pb2 as msg
 import chai.transExplorer_pb2_grpc as service
 
 T = TypeVar("T")
 
 
-class Chai:
+class NoServerConnection(Exception):
+    """Raised if client cannot connect to server after timeout expires"""
+
+
+class Chai(Awaitable):
     """Client for Human-Apalache Interaction
 
     This class implements the contextmanager protocol, and is meant to be used in
@@ -25,22 +40,23 @@ class Chai:
     Example usage:
 
     ```
-    import chai
+    from chai import Chai
 
-    with chai.Chai() as client:
+    with Chai.create() as client:
+        assert client.is_connected()
         # TODO: Add key method invocations
-        pass
     ```
 
     If you need to use the class outside of a `with` statement, be sure to
     obtain a connection before doing your work and to close the client when done:
 
     ```
+    client = Chai()
     try:
-      client = chai.Chai().connect()
-      # Do stuff
+        client.connect()
+        # Do more stuff
     finally:
-      client.close()
+        client.close()
     ```
 
     All methods aside from `connect` assume a connection has been obtained.
@@ -50,39 +66,111 @@ class Chai:
     DEFAULT_DOMAIN = "localhost"
     DEFAULT_PORT = 8822
 
-    def __init__(self, ip: str = DEFAULT_DOMAIN, port: int = DEFAULT_PORT) -> None:
-        self._channel = grpc.insecure_channel(f"{ip}:{port}")
-        self._conn: Optional[msg.Connection] = None
+    # The `create` class method lets us use grpcio.aio's async context manager
+    # to safely manage the state of the channel, and provide the user with an
+    # instance of the Chai client in that context.
+    @classmethod
+    @asynccontextmanager
+    async def create(cls, *args: Any, **kwargs: Any) -> AsyncIterator[Chai]:
+        """Async context manager to create a Chai client with managed resources
+
+        This is the recommended way to create a client, as it ensures that the
+        connections and any other resources are cleaned up on exit.
+
+        Example usage:
+
+        ```
+        async with Chai.create() as client:
+            # interact with the server
+        ```
+        """
+        client = cls(*args, **kwargs)
         try:
-            self._stub = service.TransExplorerStub(self._channel)
-        except Exception as e:
-            self.close()
-            raise e
+            async with aio.insecure_channel(client._channel_spec) as channel:
+                await client.connect(channel)
+                yield client
+        finally:
+            # To ensure any resources besides the channel are also cleaned up
+            await client.close()
 
-    def __enter__(self) -> Chai:
-        return self.connect()
+    def __init__(
+        self,
+        domain: str = DEFAULT_DOMAIN,
+        port: int = DEFAULT_PORT,
+        timeout: float = 60.0,
+    ) -> None:
+        """Initialize the Chai client.
 
-    def __exit__(self, type: Type[T], value: T, traceback: TracebackType) -> None:
-        # Unused variables
-        _ = (type, value, traceback)
-        self.close()
+        Args:
 
-    def connect(self) -> Chai:
+            domain: domain name or IP where the Apalache server is running
+            port: port to which the Apalache server is connected
+            timeout: how long to wait before giving up when trying to connect to
+                the server (default: 60 seconds)
+        """
+        self._channel_spec = f"{domain}:{port}"
+
+        self._timeout = timeout
+
+        self._channel: Optional[aio.Channel] = None
+        self._stub: Optional[service.TransExplorerStub] = None
+        self._conn: Optional[msg.Connection] = None
+
+    # We need the client to implement the await protocol
+    def __await__(self):
+        async def closure():
+            return self
+
+        return closure().__await__()
+
+    async def connect(self, channel: Optional[aio.Channel] = None) -> Chai:
         """Obtain a connection from the server
 
         All other methods assume a connection has been obtained. This method is
         called automatically when the class is used as a context manager.
+
+        If you call this method directly, you should be sure to call
+        `self.close()` to ensure the connection and channel is
         """
-        self._conn: Optional[msg.Connection] = self._stub.OpenConnection(
-            msg.ConnectRequest()
+        if channel is None:
+            # No channel is provided, so we create an unmanaged channel,
+            # which the caller must close via `self.close()`
+            self._channel = aio.insecure_channel(self._channel_spec)
+        else:
+            # We assume the caller is managing the channel (i.e., via a `with`
+            # statement)
+            self._channel = channel
+
+        self._stub = service.TransExplorerStub(self._channel)
+
+        req = msg.ConnectRequest()
+
+        # Set up a timer so we can timeout if no connection is obtained in time
+        loop = asyncio.get_running_loop()
+        end_time = loop.time() + self._timeout
+        while loop.time() < end_time:
+            try:
+                self._conn = await self._stub.OpenConnection(req)  # type: ignore
+                return self
+            except aio.AioRpcError:
+                # We weren't able to establish a connection this try
+                continue
+        else:
+            raise NoServerConnection(f"after {self._timeout} seconds")
+
+    def is_connected(self) -> bool:
+        """True if the client has an open connection on a ready channel"""
+        return (
+            self._conn is not None
+            and self._channel is not None
+            and self._channel.get_state() is ChannelConnectivity.READY
         )
-        return self
 
-    def isConnected(self) -> bool:
-        """True if the client has an open connection"""
-        return self._conn is not None
-
-    def close(self) -> None:
+    async def close(self) -> None:
         """Close the client, cleaning up connections and channels"""
+        if (
+            self._channel is not None
+            and self._channel.get_state() is not ChannelConnectivity.SHUTDOWN
+        ):
+            await self._channel.close()
         # TODO: Send RPC to terminate connection (just a courtesy for the server)
-        self._channel.close()
--- integration-tests/test_apalache_integration.py
+++ integration-tests/test_apalache_integration.py
@@ -1,41 +1,11 @@
 from __future__ import annotations
 
-from collections.abc import Iterator
+from collections.abc import AsyncIterator, Iterator
+from subprocess import Popen
 
 import pytest
-import socket
-import time
 
-from subprocess import Popen
-from chai import Chai
-
-
-# Utility function
-def wait_for_server(server: str, port: int, timeout: int) -> bool:
-    """Ping a server
-
-    Adapated from https://stackoverflow.com/a/67217558/1187277
-    """
-    remaining_time = timeout
-    while remaining_time > 0:
-        socket.setdefaulttimeout(remaining_time)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            s.connect((server, port))
-        except TimeoutError:
-            # We ran out of time waiting for a connection on the socket
-            return False
-        except OSError:
-            # We weren't able to connect to the socket, wait a bit, and try again
-            remaining_time -= 1
-            time.sleep(1)
-        else:
-            # We connected!
-            s.close()
-            return True
-    else:
-        # We ran out of remaining_time, so give up
-        return False
+from chai import Chai, NoServerConnection
 
 
 # Fixture to start and clean up Apalache's Shai server
@@ -54,12 +24,6 @@ def wait_for_server(server: str, port: int, timeout: int) -> bool:
 def server() -> Iterator[Popen]:
     # TODO Pass port to server explicitly when that is supported
     process = Popen(["apalache-mc", "server"])
-    # Startup can take quite some time, especially on the CI machines
-    timeout_secs = 60
-    if not wait_for_server(Chai.DEFAULT_DOMAIN, Chai.DEFAULT_PORT, timeout_secs):
-        raise RuntimeError(
-            f"Apalache server did not start after {timeout_secs} seconds"
-        )
     yield process
     process.terminate()
 
@@ -69,12 +33,22 @@ def server() -> Iterator[Popen]:
 # NOTE: In contrast to the `server` fixture, we do want to create this once for
 # each test
 @pytest.fixture
-def client(server: Popen) -> Iterator[Chai]:
+async def client(server: Popen) -> AsyncIterator[Chai]:
     # We need to ensure the server is created before we create the client
     _ = server
-    with Chai() as client:
+    async with Chai.create() as client:
         yield client
 
 
-def test_can_obtain_a_connection(client: Chai) -> None:
-    assert client.isConnected()
+async def test_can_obtain_a_connection(client: Chai) -> None:
+    assert client.is_connected()
+
+
+async def test_raises_error_after_timeout() -> None:
+    # configure the client to use a non-existent server
+    client = Chai(domain="invalid.domain", port=6666, timeout=0.5)
+    try:
+        with pytest.raises(NoServerConnection):
+            await client.connect()
+    finally:
+        await client.close()
